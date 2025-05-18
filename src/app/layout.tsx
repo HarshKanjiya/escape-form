288dbeed8ac09cb9349c3d9893729b61bbb659b7
Harsh Kanjiya <harshkanjiya001@gmail.com>
2025-05-18 23:58:31 +0530

    initial setup

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -26,7 +26,7 @@ export default function RootLayout({
   return (
     <html lang="en" className="dark" style={{ colorScheme: "dark" }}>
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased bg-base-50`}
       >
         <ThemeProvider
           attribute="class"
