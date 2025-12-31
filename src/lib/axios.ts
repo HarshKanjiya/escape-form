ca713266d555baf94177d5c28fd0615e8af828c4
Harsh Kanjiya <harshkanjiya001@gmail.com>
2026-01-01 00:48:34 +0530

    env var name change

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -25,7 +25,7 @@ const processQueue = (error: any, token: string | null = null) => {
 };
 
 const api = axios.create({
-    baseURL: process.env.BASE_URL || 'http://localhost:3000/api/',
+    baseURL: process.env.NEXT_PUBLIC_BASE_URL || 'http://localhost:3000/api/',
     timeout: 10000,
     headers: {
         'Content-Type': 'application/json',
